b832d83b747e4d75a85be9d43c8e525feccde641
Rohit <rohittanger2@gmail.com>
2026-07-12 12:29:23 +0530

    Time: 29 ms (84.49%), Space: 75.3 MB (63.77%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        if (n == 0) return new int[0];
-        int []arr2 = new int[n];
-        for(int i=0;i<arr.length;i++){
-            arr2[i]=arr[i];
-        }
-        int index=0;
+        int n =arr.length;
+        int []arr2 =arr.clone();
         Arrays.sort(arr2);
-        int []ranks = new int[n];
-        ranks[0]=1;
-       for(int i=1;i<n;i++){
-            if(arr2[i]==arr2[i-1]){
-                ranks[i]=ranks[i-1];
+        HashMap<Integer,Integer> map = new HashMap<>();
+//int []ranks=new int[n];
+        int rank=1;
+        for(int val:arr2){
+            if(!map.containsKey(val)){
+            map.put(val,rank++);
             }
-            else{
-                ranks[i]=ranks[i-1]+1;
-            }
-       }
-       int []res = new int[n];
-       for(int i=0;i<n;i++){
-        int pos =Arrays.binarySearch(arr2,arr[i]);
-        res[i]=ranks[pos];
-       }
-        return res;
+        }
+    int [] res =new int[n];
+    for(int i=0;i<n;i++){
+        res[i]=map.get(arr[i]);
+    }
+    return res;
     }
 }
