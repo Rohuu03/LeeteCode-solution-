1827dd80763fb9a86134c9c163a61a809a6016b9
Rohit <rohittanger2@gmail.com>
2026-07-17 19:20:36 +0530

    Time: 46 ms (26.69%), Space: 47.8 MB (12.71%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,21 +1,42 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
-        int n = s.length();
-        int k = p.length();
+        if (s.length() < p.length())
+            return res;
 
-        int [] freq = new int[26];
-        for(int i=0;i<k;i++){
+        Map<Character, Integer> map_p = new HashMap<>();
+        Map<Character, Integer> map_s = new HashMap<>();
+
+        // build frequency map for p
+        for (int i = 0; i < p.length(); i++) {
             char ch = p.charAt(i);
-            freq[ch-'a']++;
+            map_p.put(ch, map_p.getOrDefault(ch, 0) + 1);
         }
-        for(int i=0;i<=n-k;i++){
-            int freq_s[] =new int[26];
-            for(int j=i;j<i+k;j++){
-                freq_s[s.charAt(j)-'a']++;
+
+        int l = 0, count = p.length();
+
+        for (int r = 0; r < s.length(); r++) {
+            char ch = s.charAt(r);
+            map_s.put(ch, map_s.getOrDefault(ch, 0) + 1);
+
+            if (map_p.containsKey(ch) && map_s.get(ch) <= map_p.get(ch)) {
+                count--;
             }
-            if(Arrays.equals(freq,freq_s))
-            res.add(i);
+
+            // FIX: window size check
+            if (r - l + 1 > p.length()) {
+                char leftch = s.charAt(l);
+                if (map_p.containsKey(leftch) && map_s.get(leftch) <= map_p.get(leftch)) {
+                    count++;
+                }
+                map_s.put(leftch, map_s.get(leftch) - 1);
+                l++;
+            }
+
+            // when all chars matched
+            if (count == 0) {
+                res.add(l);
+            }
         }
         return res;
     }
