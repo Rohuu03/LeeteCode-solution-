ae5ace483f490d51fe49c9c8b9fdaa6ed1dbf184
Rohit <rohittanger2@gmail.com>
2026-09-04 19:28:56 +0530

    Time: 0 ms (100%), Space: 48.2 MB (31.99%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-       int first =-1;
-      int last =-1;
-        int i =0,j=n-1;
-        while(i<=j){
-            int mid = i+(j-i)/2;
-            if(nums[mid]==target){
-                first =mid;
-                j = mid-1;
-                }
-            else if(nums[mid]<target)
-                i =mid+1;
-                else{
+        int arr[] = {-1,-1};
+        
+            int i =0,j=nums.length-1;
+            while(i<=j){
+                int mid = i + (j-i)/2;
+                 if(nums[mid]==target){
+                    arr[0]=mid;
                     j=mid-1;
-                }
-        }
-         i = 0;
-        j = n - 1;
-         while(i<=j){
-            int mid = i+(j-i)/2;
-            if(nums[mid]==target){
-                last =mid;
-                i = mid+1;
-                }
+                 }
+                 else if(nums[mid]<target){
+                    i =mid+1;
+                 }
+                 else{
+                    j = mid-1;
+                 }
+            }
             
-            else if(nums[mid]<target)
-                i =mid+1;
-                else{
-                    j=mid-1;
-                }
-        }
-
-        return  new int []{first,last};
+             i =0;
+             j=nums.length-1;
+            while(i<=j){
+                int mid = i + (j-i)/2;
+                 if(nums[mid]==target){
+                    arr[1]=mid;
+                    i=mid+1;
+                 }
+                 else if(nums[mid]<target){
+                    i =mid+1;
+                 }
+                 else{
+                    j = mid-1;
+                 }
+            }
+            return arr;
     }
 }
