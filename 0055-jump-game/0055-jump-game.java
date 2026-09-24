79058b94bc48c75812fafcf5c0c027e379851f02
Rohit <rohittanger2@gmail.com>
2026-09-24 08:27:28 +0530

    Time: 2 ms (89.66%), Space: 48.1 MB (23.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,10 @@
 class Solution {
-    static boolean solve(int []nums,int i ,int max){
-        if(max>=nums.length-1)
-        return true;
-        if(i>max)
-        return false;
-        max = Math.max(max,i+nums[i]);
-      return  solve(nums,i+1,max);
-        
-    }
     public boolean canJump(int[] nums) {
-        int max =0;
-        int i=0;
-        return  solve(nums,i,max);
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
     }
 }
